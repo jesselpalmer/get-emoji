3bd57c57ceb853526fed6616455bc18927d5287b
Jesse Palmer <jesselpalmer@gmail.com>
2021-04-05 23:55:08 -0700

    feat: add redis

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const redis = require("redis")
 const axios = require('axios')
 
 const getEmojis = async () => {
@@ -11,19 +12,46 @@ const getEmojis = async () => {
 
 const app = express()
 const port = process.env.PORT || 3000
+const client = redis.createClient(process.env.REDIS_URL)
+
+client.on('error', error => {
+  console.error(error)
+})
 
 app.use(express.json())
 
-app.get('/', async (req, res) => {
+const processRequest = (emojiName, res) => {
+  client.get(emojiName, async (err, emoji) => {
+    if (err) throw err
+
+    if (emoji) {
+      console.log('cache hit')
+      res.send(emoji)
+    } else {
+      console.log('cache miss')
+      const emojis = await getEmojis()
+      const emoji = emojis.data[emojiName]
+
+      client.set(emojiName, emoji, err => {
+        if (err) throw err
+        res.send(emoji)       
+      })
+    }
+  })
+}
+
+app.get('/', (req, res) => {
   const emojiName = req.body.emojiName
-  const emojis = await getEmojis()
-  res.json(emojis.data[emojiName] || {})
+  if (!emojiName) res.send(null)
+
+  processRequest(emojiName, res)
 })
 
-app.get('/:emojiName', async (req, res) => {
+app.get('/:emojiName', (req, res) => {
   const emojiName = req.params.emojiName
-  const emojis = await getEmojis()
-  res.json(emojis.data[emojiName] || {})
+  if (!emojiName) res.send(null)
+
+  processRequest(emojiName, res)
 })
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
